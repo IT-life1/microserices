8c437defa756c4f48f2866f396e3364e5a9d5cdb
sanya <sanya.mozgov@inbox.ru>
2025-05-07 14:39:42 +0300

    main

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8080"; // заменить на ваш хост при деплое
+const API_URL = "http://gateway:8080"; // заменить на ваш хост при деплое
 
 const apiClient = axios.create({
   baseURL: API_URL,
